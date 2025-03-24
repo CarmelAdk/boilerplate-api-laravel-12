704b8514b5c9e7ed8381ec99704d868ff335a8c7
cadikin <c.adikin@denucom.com>
2025-03-24 18:40:40 +0100

    chore: add swagger and config it

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -2,7 +2,39 @@
 
 namespace App\Http\Controllers;
 
-abstract class Controller
+use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
+use Illuminate\Foundation\Validation\ValidatesRequests;
+use Illuminate\Routing\Controller as BaseController;
+
+/**
+ * @OA\Info(
+ *     version="v1",
+ *     title="Boilerplate API Documentation",
+ *     description="Documentation de l'API Boilerplate",
+ *     @OA\Contact(
+ *         email="contact@boilerplate.com"
+ *     )
+ * )
+ * @OA\Server(
+ *     url=L5_SWAGGER_CONST_HOST,
+ *     description="Serveur API"
+ * )
+ * @OA\SecurityScheme(
+ *     type="http",
+ *     scheme="bearer",
+ *     bearerFormat="JWT",
+ *     securityScheme="bearerAuth"
+ * )
+ * @OA\Tag(
+ *     name="Boilerplate",
+ *     description="API pour Boilerplate"
+ * )
+ * @OA\PathItem(
+ *     path="/v1"
+ * )
+ */
+
+abstract class Controller extends BaseController
 {
-    //
+    use AuthorizesRequests, ValidatesRequests;
 }

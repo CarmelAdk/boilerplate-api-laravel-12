544bc60821958feb990c6b50f7954dc5380f1cf9
Carmel Adikin <85707325+CarmelAdk@users.noreply.github.com>
2025-06-06 16:07:56 +0100

    Update AppServiceProvider.php

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,9 +3,12 @@
 namespace App\Providers;
 
 use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Route;
 use Illuminate\Support\ServiceProvider;
 use Illuminate\Validation\Rules\Password;
-
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\App;
+use Illuminate\Support\Facades\Log;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -22,15 +25,66 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        DB::prohibitDestructiveCommands($this->app->isProduction());
+        $this->configurePasswordRules();
+        $this->configureCommands();
+        $this->configureBlueprints();
+        $this->configureModels();
 
+        if(App::isLocal()) {
+            DB::enableQueryLog();
+
+            Route::macro('log', function () {
+                dd(DB::getQueryLog());
+            });
+
+            DB::whenQueryingForLongerThan(1, function ($connection) {
+                Log::warning(
+                    'Query took longer than 1 second.',
+                    $connection->getQueryLog()
+                );
+            });
+        }
+    }
+
+    /**
+     * Configure the default password validation rules.
+     */
+    private function configurePasswordRules(): void
+    {
         Password::defaults(function () {
             $rule = Password::min(8);
 
-            return $this->app->isProduction()
+            return App::isProduction()
                         ? $rule->mixedCase()->numbers()->symbols()->uncompromised()
                         : $rule;
         });
+    }
 
+    /**
+     * Disable destructive database commands in production.
+     *
+     * This enforces "read-only" mode for the database in production.
+     *
+     */
+    private function configureCommands(): void
+    {
+        DB::prohibitDestructiveCommands(App::isProduction());
+    }
+
+    /**
+     * Configure Eloquent models.
+     *
+     */
+    private function configureModels(): void
+    {
+        Model::unguard();
+        Model::shouldBeStrict();
+    }
+
+    private function configureBlueprints(): void
+    {
+        // Blueprint::macro('organization', function () {
+        //     return $this->foreignId('organization_id')->constrained()->cascadeOnDelete();
+        // });
     }
 }
